c1a637a4b8357da7f99b5fb1426eee95661f3891
Charlie Pank <charlie@pank.org.uk>
2025-09-01 23:44:10 +1200

    new lib should handle decimal for max_gas_price_multiplier

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 
 dependencies {
     // Blockchain Relay Utility from Jitpack
-    implementation("com.github.charliepank:blockchain-relay-utility:v3.0.3")
+    implementation("com.github.charliepank:blockchain-relay-utility:v3.0.4")
     
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
