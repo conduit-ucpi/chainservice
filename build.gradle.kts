ef24a5afb2ea6ad9ddc9bc916ed8376445db42fd
Charlie Pank <charlie@pank.org.uk>
2025-09-01 14:54:50 +1200

    new version of library with contract fee payer

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 
 dependencies {
     // Blockchain Relay Utility from Jitpack
-    implementation("com.github.charliepank:blockchain-relay-utility:v0.3.6")
+    implementation("com.github.charliepank:blockchain-relay-utility:v1.0.1")
     
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
