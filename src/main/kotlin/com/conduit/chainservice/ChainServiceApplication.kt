974f4e0b20680d9534fb0fd3d18c4056fd626a90
Charlie Pank <charlie@pank.org.uk>
2025-09-04 15:57:19 +1200

    more security config to get rid of api keys

--- src/main/kotlin/com/conduit/chainservice/ChainServiceApplication.kt
+++ src/main/kotlin/com/conduit/chainservice/ChainServiceApplication.kt
@@ -9,7 +9,7 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration::class])
 @EnableConfigurationProperties(EscrowProperties::class, AuthProperties::class, BlockchainProperties::class)
 @EnableScheduling
 @ComponentScan(basePackages = ["com.conduit.chainservice", "com.utility.chainservice"])
